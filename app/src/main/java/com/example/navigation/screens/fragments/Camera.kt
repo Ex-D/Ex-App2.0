9c76efbf61a8f7202c55dd753c1ba812ac6c86f6
Rohit-554 <rohitkumarccc68@gmail.com>
2023-03-29 04:30:25 +0530

    bgRemove added

--- app/src/main/java/com/example/navigation/screens/fragments/Camera.kt
+++ app/src/main/java/com/example/navigation/screens/fragments/Camera.kt
@@ -15,8 +15,12 @@ import android.view.ViewGroup
 import androidx.annotation.RequiresApi
 import androidx.core.net.toUri
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import com.example.navigation.R
 import com.example.navigation.databinding.FragmentCameraBinding
+import com.google.android.material.snackbar.Snackbar
+import com.slowmac.autobackgroundremover.BackgroundRemover
+import com.slowmac.autobackgroundremover.OnBackgroundChangeListener
 import ja.burhanrashid52.photoeditor.PhotoEditorView
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
@@ -32,11 +36,11 @@ class Camera : Fragment() {
     private lateinit var bundle: Bundle
     private lateinit var intent: Intent
     private lateinit var uri: Uri
+    private lateinit var snackbar: Snackbar
     private lateinit var bitmap: Bitmap
     private lateinit var contentResolver: ContentResolver
     private lateinit var matrix: Matrix
 
-    @RequiresApi(Build.VERSION_CODES.P)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -46,114 +50,45 @@ class Camera : Fragment() {
 
         uri = intent.getStringExtra("imageUri")?.toUri()!!
         _binding = FragmentCameraBinding.inflate(inflater, container, false)
-
-        val inputStream: InputStream? = activity?.contentResolver?.openInputStream(uri)
-        var yourDrawable = Drawable.createFromStream(inputStream, uri.toString())
-
-
-        val mPhotoEditorView: PhotoEditorView = binding.photoEditorView
-
-        GlobalScope.launch(Dispatchers.Main) {
-            val test = withContext(Dispatchers.IO) {
-                ImageDecoder.decodeBitmap(
-                    ImageDecoder.createSource(
-                        requireContext().contentResolver,
-                        uri
-                    )
-                )
-            }
-
-//            val bmp = getBitmapFromView(binding.editImage)
-
-//            val bmp: Bitmap = Bitmap.createBitmap(binding.editImage.getDrawingCache())
-//            Log.d("editbitmap","$bitmap_1")
-
-            binding.removebg.setOnClickListener {
-//                combineImages(test, bmp)
-//                binding.gifImage.setImageBitmap(combineImages(test,bmp))
-//                binding.gifImage.invalidate()
-//                BackgroundRemover.bitmapForProcessing(
-//                    test, true,
-//                    object : OnBackgroundChangeListener {
-//                        override fun onSuccess(bitmap: Bitmap) {
-//                            //do what ever you want to do with this bitmap
-//
-//                            binding.gifImage.setImageBitmap(bitmap)
-//                            Log.d("imagetesting", "${bitmap}")
-//                        }
-//
-//                        override fun onFailed(exception: Exception) {
-//                            Toast.makeText(
-//                                requireContext(),
-//                                "Something went wrong",
-//                                Toast.LENGTH_SHORT
-//                            ).show()
-//                        }
-//                    }
-//                )
-
-
-                //here is code 1
-
-            }
-        }
-
-//        getBitmap()
-
+        removeBg(uri)
 
         return binding.root
     }
 
-    private fun combineImages(background: Bitmap?, foreground: Bitmap?): Bitmap {
-        val matrix = Matrix()
-        val rect = Rect()
-        rect.set(0,0,100,100)
-
-//        matrix.setValues(floatArrayOf(1f, .5f, 0f, 0f, 1f, 0f, 0f, 0f, 1f))
-        var width = 0
-        var height = 0
-        width = background!!.width
-        height = background.height
-        matrix.postScale(width.toFloat(), height.toFloat())
-        val cs: Bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
-        val comboImage = Canvas(cs)
-
-        val storage = Bitmap.createScaledBitmap(background, width, height, true)
-
-        comboImage.drawBitmap(storage,matrix,null)
-//        comboImage.drawBitmap(storage,rect, null)
-        if (foreground != null) {
-            comboImage.drawBitmap(storage,matrix,null)
-
-        }
-        return cs
-    }
-
-
-    fun getBitmapFromView(view: View): Bitmap {
-        val bitmap = Bitmap.createBitmap(
-            view.width, view.height, Bitmap.Config.ARGB_8888
-        )
-        val canvas = Canvas(bitmap)
-        view.draw(canvas)
-        return bitmap
-    }
-    fun getBitmap(): Bitmap {
-        Thread(Runnable {
-            // a potentially time consuming task
-            val bitmap = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-                ImageDecoder.decodeBitmap(
-                    ImageDecoder.createSource(
-                        requireContext().contentResolver,
-                        uri
-                    )
+    private fun removeBg(uri: Uri){
+        lifecycleScope.launch(Dispatchers.IO){
+            val newbitmap:Bitmap? = getBitmap(uri)
+            if (newbitmap != null) {
+                BackgroundRemover.bitmapForProcessing(
+                    newbitmap,
+                    true,
+                    object: OnBackgroundChangeListener {
+                        override fun onSuccess(bitmap: Bitmap) {
+                            //bitmap
+                            binding.gifImage.setImageBitmap(bitmap)
+                        }
+                        override fun onFailed(exception: Exception) {
+                            displaySnackBar("Failed to remove background")
+                        }
+                    }
                 )
-            } else {
-                MediaStore.Images.Media.getBitmap(requireContext().contentResolver, uri)
             }
-        }).start()
-        return bitmap
+        }
     }
 
+    fun getBitmap(uri: Uri): Bitmap? {
+        val inputStream: InputStream? = activity?.contentResolver?.openInputStream(uri)
+        return BitmapFactory.decodeStream(inputStream)
+    }
 
+    private fun displaySnackBar(text:String){
+        snackbar = view?.let { it1 ->
+            Snackbar.make(
+                it1,
+                text,
+                Snackbar.LENGTH_LONG
+            )
+        }!!
+        snackbar.show()
+    }
 }
